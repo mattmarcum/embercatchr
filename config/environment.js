826cc276e917e73e91f41661f48c44bc0ece4887
Matt <matt@strangelooper.com>
2014-12-13 00:11:19 -0600

    added ga, and less compilation

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.googleAnalytics = {
+      webPropertyId: 'UA-57592533-1'
+    };
   }
 
   return ENV;
